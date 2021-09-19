d0bdf96c05365d664b4a5a7411aac43aed3f600d
jselbaz <jselbaz@gmail.com>
2021-09-18 21:08:28 -0500

    finished problems 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
-
+	let reversed = [];
+	for (let i=0; i<str.length;i++) {
+		reversed.push(str[i])
+	}
+	return reversed.reverse().join('');
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, num1, ...num2){
+	for (let i=1; i<=arguments.length; i++) {
+		if (arr.includes(arguments[i])) {
+			let index = arr.indexOf(arguments[i])
+			arr.splice(index,1);
+		}
+	}
+	return arr;
 };
 
 // Do not edit below this line
